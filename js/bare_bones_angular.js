170aa8e33079fc2e019899770c47416b0a6c5df9
Patrick Silva <patricksssilva@yahoo.com>
2019-04-24 14:04:34 +0800

    Worked on Angular barebones

--- js/bare_bones_angular.js
+++ js/bare_bones_angular.js
@@ -1,94 +1,76 @@
+
+/**
+Functionality for the Client side of the System
+*/
+
 var requestModule=angular.module('requestApp');
+requestModule.controller=controller("requestController",['$compile', '$scope',"$http","$rootScope", function requestController($compile, $scope,$http,$rootScope){
+
+	var request_id=$rootScope.user_id;
+	var token=$rootScope.token;
+	
+	$http.get("http://localhost/lnf_api/requests/userRequests/"+request_id+"/?api_token="+token).then(function(response) {$scope.requests = response.requests;});	
 
 
-requestModule.controller=controller("requestController",['$compile', '$scope',"$http", function requestController($compile, $scope,$http){
 	$scope.listUserRequests=function (){
-		var user_id=$scope.user_id;
-		$http.get("http://localhost/lnf_api/requests/userRequests/"+user_id).then(function(response) {$scope.requests = response.requests;});	
+		var request_id=$scope.request_id;
+		$http.get("http://localhost/lnf_api/requests/userRequests/"+request_id+"/?api_token="+token).then(function(response) {$scope.requests = response.requests;});	
 	
 	};
 	$scope.retrieveItem=function (){
 		
-		var item_id=$scope.item_id;
-		$http.get("http://localhost/lnf_api/request/"+item_id).then(function(response) {$scope.requests = response.requests;});	
+		var request_id=$scope.request_id;
+		$http.get("http://localhost/lnf_api/request/"+request_id+"/?api_token="+token).then(function(response) {$scope.requests = response.requests;});	
 
 	};
 	
 	$scope.retrieveRequestStatus=function (){
 		
-		var item_id=$scope.item_id;
-		$http.get("http://localhost/lnf_api/request/"+item_id+"/status").then(function(response) {$scope.requests = response.requests;});	
+		var request_id=$scope.request_id;
+		$http.get("http://localhost/lnf_api/request/"+item_id+"/status/?api_token="+token).then(function(response) {$scope.requests = response.requests;});	
 
 	};
 
-
-	$http.get("http://localhost/lnf_api/requests/userRequests/1").then(function(response) {$scope.requests = response.data; });	
 		  
 }]);
 
 var requestDetailsModule=angular.module('detailsApp');
+requestDetailsModule.controller=controller("requestDetailsController",['$compile','$scope',"$http","$rootScope", function requestDetailsController($compile, $scope,$http,$rootScope){
 
-requestDetailsModule.controller=controller("requestDetailsController",['$compile','$scope',"$http", function requestDetailsController($compile, $scope,$http){
-
-	$http.get("http://localhost/lnf_api/request/1").then(function(response) {$scope.requests = response.data; });	
+	var item_id=$scope.item_id;
+	var token=$rootScope.token;
+	$http.get("http://localhost/lnf_api/request/"+request_id+"/?api_token="+token).then(function(response) {$scope.requests = response.data; });	
 		  
 }]);
 
+/**
+Admin Side, for Listing of Items
+*/
 
-var addRequestModule=angular.module('addRequestApp');
 
-addRequestModule.controller=controller("addRequestController",['$compile', '$scope',"$http", function addController($compile, $scope,$http){
+var itemListModule=angular.module('itemListApp');
+itemListModule.controller=controller("itemListController",['$compile','$scope',"$http","$rootScope", function itemListController($compile, $scope,$http,$rootScope){
 
 
-	var url="http://localhost/lnf_api/request";
-	
-	
-	
-	var parameter = JSON.stringify({type:"user", username:user_email, password:user_password});
-    $http.post(url, parameter).
-    success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
+	//Introduce Tagging
+	var token=$rootScope.token;
 
-//	$http.get("http://localhost/lnf_api/request/1").then(function(response) {$scope.requests = response.data; });	
+
+	var category=$scope.category;
+	var search_id=$scope.search_id;
+
+
+	$http.get("http://localhost/lnf_api/items/"+category+"/"+search_id+"/?api_token="+token).then(function(response) {$scope.items = response.data; });	
+
+	$scope.retrieveItemStatus=function (){
+		
+		var item_id=$scope.item_id;
+		$http.get("http://localhost/lnf_api/item/"+item_id+"/status/?api_token="+token).then(function(response) {$scope.requests = response.requests;});	
+
+	};
 		  
 }]);
 
-
-
-
-
-
-var addItemModule=angular.module('addItemApp');
-
-addItemModule.controller=controller("addController",['$compile', '$scope',"$http", function addController($compile, $scope,$http){
-
-	
-	
-	var url="http://localhost/lnf_api/item";
-	
-	
-	
-	var parameter = JSON.stringify({type:"user", username:user_email, password:user_password});
-    $http.post(url, parameter).
-    success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-
-		  
-}]);
 
 
 var itemDetailsModule=angular.module('itemDetailsApp');
@@ -96,13 +78,15 @@ var itemDetailsModule=angular.module('itemDetailsApp');
 itemDetailsModule.controller=controller("itemDetailsController",['$compile','$scope',"$http", function itemDetailsController($compile, $scope,$http){
 
 	var item_id=$scope.item_id;
-		
-	$http.get("http://localhost/lnf_api/item/"+item_id).then(function(response) {$scope.requests = response.data; });	
+	var token=$rootScope.token;
+
+	
+	$http.get("http://localhost/lnf_api/item/"+item_id+"/?api_token="+token).then(function(response) {$scope.requests = response.data; });	
 
 	$scope.retrieveItemStatus=function (){
 		
 		var item_id=$scope.item_id;
-		$http.get("http://localhost/lnf_api/item/"+item_id+"/status").then(function(response) {$scope.requests = response.requests;});	
+		$http.get("http://localhost/lnf_api/item/"+item_id+"/status/?api_token="+token").then(function(response) {$scope.requests = response.requests;});	
 
 	};
 	
@@ -110,54 +94,106 @@ itemDetailsModule.controller=controller("itemDetailsController",['$compile','$sc
 
 
 
-var itemListModule=angular.module('itemListApp');
+/**
+For both Client Side and Admin side, for adding Items
 
-itemListModule.controller=controller("itemListController",['$compile','$scope',"$http", function itemListController($compile, $scope,$http){
-
-
-	//Introduce Tagging
+*/
 
 
-	var category=$scope.category;
-	var search_id=$scope.search_id;
+var addRequestModule=angular.module('addRequestApp');
+addRequestModule.controller=controller("addRequestController",['$compile', '$scope',"$http", function addController($compile, $scope,$http){
 
 
-	$http.get("http://localhost/lnf_api/items/"+category+"/"+search_id).then(function(response) {$scope.items = response.data; });	
+	var url="http://localhost/lnf_api/request/?api_token"+token;
+	
+	var parameter = JSON.stringify({
+						type:"user", 
+						username:user_email, 
+						password:user_password
+					});
+    $http.post(url, parameter).
+    success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.response=data;
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
 
-	$scope.retrieveItemStatus=function (){
-		
-		var item_id=$scope.item_id;
-		$http.get("http://localhost/lnf_api/item/"+item_id+"/status").then(function(response) {$scope.requests = response.requests;});	
+		  
+}]);
 
-	};
+
+var addItemModule=angular.module('addItemApp');
+addItemModule.controller=controller("addController",['$compile', '$scope',"$http", function addController($compile, $scope,$http){
+
+	
+	
+	var url="http://localhost/lnf_api/item/?api_token"+token;
+	
+	var parameter = JSON.stringify({
+						type:"user", 
+						username:user_email, 
+						password:user_password
+					});
+    $http.post(url, parameter).
+    success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.response=data;
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
 		  
 }]);
 
 
 
+/**
+For login/authentication
+*/
+
 
 var loginModule=angular.module('loginApp');
 
-loginModule.controller=controller("loginController",['$compile','$scope',"$http", function loginController($compile, $scope,$http){
+loginModule.controller=controller("loginController",['$compile','$scope',"$http", function loginController($compile, $scope,$http,$rootScope){
 
 
-	//Introduce Tagging
 	var url="http://localhost/lnf_api/user/login";
-
+	
 	
 	$scope.loginUser=function (){
+
+		var username=$scope.username;
+		var password=$scope.password;
 	
-		var parameter = JSON.stringify({type:"user", username:user_email, password:user_password});
+		var parameter = JSON.stringify({username:username, password:password});
 		$http.post(url, parameter).
 		success(function(data, status, headers, config) {
 			// this callback will be called asynchronously
 			// when the response is available
-			
-			
+			var response=data;
+						
 			//if login is illegal
+			if(response["confirm"]==false){
+				$scope.error_message=response["message"];
+			}
+			else {
+				$scope.error_mesage=response["message"];
+				$rootScope.username=response["username"];
+				$rootScope.name=response["name"];
+				$rootScope.token=response["token"];
+				
+			}
 			
 			
-			console.log(data);
+			
+			//console.log(data);
 		}).
 		error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
@@ -176,23 +212,106 @@ var registrationModule=angular.module('registrationApp');
 
 registrationModule.controller=controller("registrationController",['$compile','$scope',"$http", function registrationController($compile, $scope,$http){
 
-	var url="http://localhost/lnf_api/register";
 
 
 	$scope.registerUser=function (){
+
+		var url="http://localhost/lnf_api/register";
 	
-		var parameter = JSON.stringify({type:"user", username:user_email, password:user_password});
+		var username=$scope.username;
+		var name=$scope.name;
+		var password=$scope.password;
+	
+	
+		var parameter = JSON.stringify({username:username, password:password, name:name});
 		$http.post(url, parameter).
 		success(function(data, status, headers, config) {
 			// this callback will be called asynchronously
 			// when the response is available
-			console.log(data);
+			
+			
+			var response=data;
+			
+			$scope.user_id=response["user_id"];
+			
+			
 		}).
 		error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 		});
+		
 	};
+	
+	$scope.enterProfileDetails=function(){
+		
+
+		var profile_parameter = JSON.stringify({
+									first_name:first_name, 
+									last_name:last_name, 
+									middle_name:middle_name
+								});
+		var address_parameter = JSON.stringify({
+									unit:unit, 
+									street:street, 
+									subdivision:subdivision, 
+									city:city, 
+									zip_code:zip_code, 
+									country_id:country_id, 
+									
+								});
+		var contact_parameter = JSON.stringify({
+									landline:landline, 
+									mobile:mobile, 
+									email:email
+								});
+
+								
+		var url="http://localhost/lnf_api/register/"+$scope.user_id+"/profile";						
+		var url2="http://localhost/lnf_api/register/"+$scope.user_id+"/address";						
+		var url3="http://localhost/lnf_api/register/"+$scope.user_id+"/contact";						
+								
+								
+			
+		$http.post(url, profile_parameter).
+		success(function(data, status, headers, config) {
+			// this callback will be called asynchronously
+			// when the response is available
+			$scope.profile_message=data["message"];
+		}).
+		error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
+		$http.post(url2, address_parameter).
+		success(function(data, status, headers, config) {
+			// this callback will be called asynchronously
+			// when the response is available
+			$scope.address_message=data["message"];
+			
+		}).
+		error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+			
+		});
+		$http.post(url3, contact_parameter).
+		success(function(data, status, headers, config) {
+			// this callback will be called asynchronously
+			// when the response is available
+			$scope.contact_message=data["message"];
+		}).
+		error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
+
+
+		
+	};
+	
+	
+	
 
 }]);
 
