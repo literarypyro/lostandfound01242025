4b8c2e8039f5f1ad56e4c53baac8a7b7b266eab8
Patrick Silva <patricksssilva@yahoo.com>
2019-05-07 16:57:27 +0800

    Finished tentative Login Page

--- js/login.js
+++ js/login.js
@@ -4,18 +4,17 @@ var loginModule=angular.module('loginApp',[]);
 loginModule.controller('loginController',['$compile','$scope',"$http","$rootScope", function loginController($compile, $scope,$http,$rootScope){
 
 	$scope.loginUser=function(){
-		alert("A");
-		var url="http://localhost/lnf_api/user/login";
+		var url="http://localhost/lnf_api_old/lnf_api/login";
 		
 		var username=$scope.username;
 		var password=$scope.password;
 		
-		var parameter = JSON.stringify({username:username, password:password});
+		var parameter = JSON.stringify({username:$scope.username, password:$scope.password});
 		$http.post(url, parameter).
-		success(function(data, status, headers, config) {
+		then(function(resp, status, headers, config) {
 			// this callback will be called asynchronously
 			// when the response is available
-			var response=data;
+			var response=resp.data;
 						
 			//if login is illegal
 			
@@ -26,17 +25,22 @@ loginModule.controller('loginController',['$compile','$scope',"$http","$rootScop
 				$scope.error_message=response["message"];
 			}
 			else {
+
+				var token=response["token"];
+
 				$scope.error_mesage=response["message"];
-				$rootScope.username=response["username"];
-				$rootScope.name=response["name"];
-				$rootScope.token=response["token"];
-				$rootScope.login_type=response["login_type"];
+//				$rootScope.username=response["username"];
 				
+				
+//				$rootScope.name=response["name"];
+//				$rootScope.token=response["token"];
+//				$rootScope.login_type=response["login_type"];
+	
 				if(response["login_type"]=="user"){
-					window.open("request_list.html");	
+					window.open("request_list.html?token="+token,"_self");	
 				}
 				else if(response["login_type"]=="administrator"){
-					window.open("admin_dashboard.html");			
+					window.open("admin_dashboard.html?token="+token,"_self");			
 				}
 			}
 			//console.log(data);
