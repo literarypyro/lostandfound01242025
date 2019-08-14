e93791874cfcd3365c391f3b3c1159f00fb9b2e2
Patrick <patricksssilva@yahoo.com>
2019-08-14 19:55:57 +0800

    Latest changes 08142019

--- js/login.js
+++ js/login.js
@@ -8,7 +8,9 @@ loginModule.controller('loginController',['$compile','$scope',"$http","$rootScop
 		
 		
 		//var url="http://192.168.1.11/lnf_api_old/lnf_api/login";
-		var url="http://10.20.5.11/lnf_api_old/lnf_api/login";
+		//var url="http://10.20.5.11/lnf_api_old/lnf_api/login";
+//		var url="http://192.168.10.7/lnf_api_old/lnf_api/login";
+		var url="http://localhost/lnf_api_old/lnf_api/login";
 		
 		var username=$scope.username;
 		var password=$scope.password;
