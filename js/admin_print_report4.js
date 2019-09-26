7d637f3c55996fb478bc792272e3204551a516cc
Patrick Silva <patricksssilva@yahoo.com>
2019-09-26 16:42:44 +0800

    Worked on table for expired items reports

--- js/admin_print_report4.js
+++ js/admin_print_report4.js
@@ -15,7 +15,58 @@ requestModule.controller("itemsController",['$compile', '$scope','$http','$windo
 	
 	$scope.period2=$filter('date')(dateTo.replace("%20"," "), "MM/yyyy");
 
+		var url2=host+"items/expired/1";
 
+		$http.get(url2).then(function(resp, status, headers, config) {
+			var response=resp.data;
+			
+			
+			
+				/*		
+			$scope.res = {};
+			response.forEach(function(v) {
+			  res[v["expired_items"][0]["category_id"]] = (res[v["expired_items"][0]["category_id"]] || 0) + 1;
+			})			
+			
+			*/
+			
+			var res=[];
+
+
+			Object.keys(response).forEach(function (key){
+
+				i=0;
+				Object.keys(response[key]["items"]).forEach(function (key2){
+					if(i==0){
+						res[response[key]["items"][key2]["category_id"]]++;
+						
+						
+						
+						
+						
+						
+						i++;
+					}
+					
+					
+				});
+		
+
+
+
+			});			
+			alert(res["1"]);
+
+			Object.keys(res).forEach(function (key){
+//				alert(key);
+			//alert(res[key]*1);
+			});
+			
+			
+			
+//			$scope.expired_items=response;		
+
+		});	
 	var url=host+"items/daterange/1/range/"+request_id;
 
 		
@@ -33,6 +84,6 @@ requestModule.controller("itemsController",['$compile', '$scope','$http','$windo
 
 		});		
 		
-
+			
 }]);
 
